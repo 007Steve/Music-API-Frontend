ad2ff835253712b9ccdf73f2832cb3241f76e0d9
007steve <stephen.plummer1.sp@gmail.com>
2020-06-26 13:51:41 -0400

    added fetch

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded",function(){
             headers: {
             'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data) 
         })
     
     }
