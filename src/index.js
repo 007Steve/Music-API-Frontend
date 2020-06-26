621a7ce9e971d67c0dae14c548dd9b914f9e2eb4
007steve <stephen.plummer1.sp@gmail.com>
2020-06-25 21:12:05 -0400

    added DOMContentLoaded

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-fetch('http://localhost:3000/songs')
-.then(res => res.json())
-.then(songs =>{ 
-    songs.forEach(song =>{
-        
-        const {id, artist, title, genre, mp3, image, album} = song
-        
-        new Song(id, artist, title, genre, mp3, image, album)
-    })
-})   
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('http://localhost:3000/songs')
+    .then(res => res.json())
+    .then(songs =>{ 
+        songs.forEach(song =>{
+            const {id, artist, title, genre, mp3, image, album} = song
+            new Song(id, artist, title, genre, mp3, image, album)
+        })
+    })   
+    });
